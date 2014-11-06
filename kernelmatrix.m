3f8dde658a1c38de6812c05b32ed1dffc077e78c
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2014-11-06 09:11:08 +0000

    Added missing 'b' and 'd' parameters for polynomial kernel. Faster RBF kernel.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@571 cf90e129-9db2-4be2-879b-6cf61410cf68

--- kernelmatrix.m
+++ kernelmatrix.m
@@ -1,11 +1,11 @@
-% function K = kernelmatrix(ker,X,X2,sigma)
+% function K = kernelmatrix(ker,X,X2,sigma,b,d)
 %
-% Inputs: 
+% Inputs:
 %	ker:    'lin','poly','rbf','sam'
 %	X:	data matrix with training samples in rows and features in columns
 %	X2:	data matrix with test samples in rows and features in columns
 %	sigma: width of the RBF kernel
-% 	b:     bias in the linear and polinomial kernel 
+% 	b:     bias in the linear and polinomial kernel
 %	d:     degree in the polynomial kernel
 %
 % Output:
@@ -16,55 +16,51 @@
 %
 % Gustavo Camps-Valls, 2006
 % Jordi (jordi@uv.es),
+%   2014-11: added (missing) b,d input parameters. Faster RBF kernel :-).
 %   2007-11: if/then -> switch, and fixed RBF kernel
 %   2010-04: RBF can be computed now also on vectors with only one feature (ie: scalars)
 
-function K = kernelmatrix(ker,X,X2,sigma)
+function K = kernelmatrix(ker,X,X2,sigma,b,d)
 
 switch ker
     case 'lin'
         if exist('X2','var')
-          K = X' * X2;
+            K = X' * X2;
         else
-          K = X' * X;
+            K = X' * X;
         end
-
+        
     case 'poly'
         if exist('X2','var')
-          K = (X' * X2 + b).^d;
+            K = (X' * X2 + b).^d;
         else
-          K = (X' * X + b).^d;
+            K = (X' * X + b).^d;
         end
-
+        
     case 'rbf'
-        if size(X,1) == 1
-            n1sq = X.^2;
-        else
-            n1sq = sum(X.^2);
-        end
+        n1sq = sum(X.^2,1);
         n1 = size(X,2);
-
+        
         if exist('X2','var');
-            if size(X2,1) == 1
-                n2sq = X2.^2;
-            else
-              n2sq = sum(X2.^2);
-            end
+            n2sq = sum(X2.^2,1);
             n2 = size(X2,2);
-            D = (ones(n2,1)*n1sq)' + ones(n1,1)*n2sq -2*X'*X2;
+            D = n1sq'*ones(1,n2) + ones(n1,1)*n2sq - 2*(X'*X2);
+            % In MATLAB, multipliying times 'ones' is faster than repmat!
+            % D = repmat(n1sq',1,n2) + repmat(n2sq,n1,1) - 2*(X'*X2);
         else
-            D = (ones(n1,1)*n1sq)' + ones(n1,1)*n1sq -2*X'*X;
+            D = 2 * (ones(n1,1)*n1sq - X'*X);
+            % D = 2 * (repmat(n1s1,n1,1) - X'*X);
         end
         K = exp(-D/(2*sigma^2));
-	
+        
     case 'sam'
         if exist('X2','var');
-          D = X'*X2;
+            D = X'*X2;
         else
-          D = X'*X;
+            D = X'*X;
         end
         K = exp(-acos(D).^2/(2*sigma^2));
-
+        
     otherwise
         error(['Unsupported kernel ' ker])
 end
