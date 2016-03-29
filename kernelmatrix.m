7e93bcefdc793d32c8069e1b315be93fd2b185f0
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-03-29 10:41:13 +0000

    kernelmatrix: added exponential, Laplace, tanh, ANOVA, rq (rational quadratic),               and mq (multiquadratic) kernels. predict (in Feature_extraction): bug fixed.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@630 cf90e129-9db2-4be2-879b-6cf61410cf68

--- kernelmatrix.m
+++ kernelmatrix.m
@@ -2,7 +2,7 @@
 %
 % Inputs:
 %	ker:    'lin','poly','rbf','sam'
-%	X:	data matrix with training samples in rows and features in columns
+%	X1: data matrix with training samples in rows and features in columns
 %	X2:	data matrix with test samples in rows and features in columns
 %	sigma: width of the RBF kernel
 % 	b:     bias in the linear and polinomial kernel
@@ -16,51 +16,163 @@
 %
 % Gustavo Camps-Valls, 2006
 % Jordi (jordi@uv.es),
-%   2014-11: added (missing) b,d input parameters. Faster RBF kernel :-).
-%   2007-11: if/then -> switch, and fixed RBF kernel
 %   2010-04: RBF can be computed now also on vectors with only one feature (ie: scalars)
+%   2007-11: if/then -> switch, and fixed RBF kernel
+%   2014-11: added (missing) b,d input parameters. Faster RBF kernel :-).
+%   2016-01: added exponential, Laplace, tanh, ANOVA, rq (rational quadratic),
+%            mq (multiquadratic) kernels
 
-function K = kernelmatrix(ker,X,X2,sigma,b,d)
+function K = kernelmatrix(ker,X1,X2,varargin) % ,sigma,b,d)
 
+% Check input arguments
 switch ker
-    case 'lin'
-        if exist('X2','var')
-            K = X' * X2;
-        else
-            K = X' * X;
-        end
-        
     case 'poly'
+        if size(varargin,2) < 2,
+            error('RBF kernel needs ''bias'' and ''degree'' parameters')
+        end
+        bias = varargin{1};
+        degree = varargin{2};
+        
+    case {'tanh','rq','mq'}
+        if size(varargin,2) < 1,
+            error('tanh kernel needs ''bias'' parameter')
+        end
+        bias = varargin{1};
+        
+    case {'pow','power'}
+        if size(varargin,2) < 1,
+            error('power kernel needs ''degree'' parameter')
+        end
+        degree = varargin{1};
+        
+    case  {'rbf','sam','exp','exponential','laplace','anova'}
+        if size(varargin,2) < 1,
+            error('RBF kernel needs ''sigma'' parameter')
+        end
+        sigma = varargin{1};
+end
+
+% Compute requested kernel
+switch ker
+    case {'lin','sam'}
         if exist('X2','var')
-            K = (X' * X2 + b).^d;
+            K = X1' * X2;
         else
-            K = (X' * X + b).^d;
+            K = X1' * X1;
+        end
+        if strcmp(ker,'sam')
+            K = exp(-acos(K).^2/(2*sigma^2));
         end
         
-    case 'rbf'
-        n1sq = sum(X.^2,1);
-        n1 = size(X,2);
-        
-        if exist('X2','var');
-            n2sq = sum(X2.^2,1);
-            n2 = size(X2,2);
-            D = n1sq'*ones(1,n2) + ones(n1,1)*n2sq - 2*(X'*X2);
-            % In MATLAB, multipliying times 'ones' is faster than repmat!
-            % D = repmat(n1sq',1,n2) + repmat(n2sq,n1,1) - 2*(X'*X2);
+    case {'poly','tanh'}
+        if exist('X2','var')
+            K = (X1' * X2 + bias);
         else
-            D = 2 * (ones(n1,1)*n1sq - X'*X);
-            % D = 2 * (repmat(n1s1,n1,1) - X'*X);
+            K = (X1' * X1 + bias);
         end
-        K = exp(-D/(2*sigma^2));
-        
-    case 'sam'
-        if exist('X2','var');
-            D = X'*X2;
+        if strcmp(ker, 'poly')
+            K = K .^ degree;
         else
-            D = X'*X;
+            K = tanh(K);
         end
-        K = exp(-acos(D).^2/(2*sigma^2));
+        
+    case 'rq'
+        if exist('X2','var')
+            K = norm2mat(X1,X2);
+        else
+            K = norm2mat(X1);
+        end
+        
+        K = 1 - K./(K + bias);
+        
+    case 'mq'
+        if exist('X2','var')
+            K = norm2mat(X1,X2);
+        else
+            K = norm2mat(X1);
+        end
+        K = sqrt( K + bias^2 );
+        
+    case {'rbf','exp','exponential','laplace'}
+        if exist('X2','var')
+            K = norm2mat(X1,X2);
+        else
+            K = norm2mat(X1);
+        end
+        switch ker
+            case 'rbf'
+                K = exp(-K/(2*sigma^2));
+            case {'exp','exponential'}
+                K = exp(-sqrt(K)/(2*sigma^2));
+            case 'laplacian'
+                K = exp(-sqrt(K)/sigma);
+        end
+        
+    case 'power'
+        if exist('X2','var')
+            K = norm2mat(X1,X2);
+        else
+            K = norm2mat(X1);
+        end
+        K = K .^ (degree/2);
+        
+    case 'anova'
+        % For each dimension compute a RBF kernel and sum them all
+        K = 0;
+        d = size(X1,1);
+        if ~exist('X2','var')
+            X2 = X1;
+        end
+        for k = 1:d
+            K = K + d * kernelmatrix('rbf', X1(k,:), X2(k,:), sigma);
+        end
+        
+    case {'chi-square','chi'}
+        if ~exist('X2','var')
+            X2 = X1;
+        end
+        
+        d = size(X1,1);
+        n1 = size(X1,2);
+        n2 = size(X2,2);
+        if (size(X2,1) ~= d)
+            error('X1 and X2 have different dimensions')
+        end
+        K = 0;
+        for k = 1:d
+            %N = 2 * X1(k,:)' * X2(k,:);
+            %D = X1(k,:)' * ones(1,n2) + ones(n1,1) * X2(k,:);
+            %K = K + N./D;
+            K = K + (2 * X1(k,:)' * X2(k,:)) ./ (X1(k,:)' * ones(1,n2) + ones(n1,1) * X2(k,:));
+        end
+        
+        % Version lenta pero segura (para comprobar)
+        %K = zeros(n1,n2);
+        %for i = 1:n1
+        %    for j = 1:n2
+        %        for k = 1:d
+        %            K(i,j) = K(i,j) + (2 * X1(k,i) .* X2(k,j)) / ( X1(k,i) + X2(k,j) );
+        %        end
+        %    end
+        %end
         
     otherwise
         error(['Unsupported kernel ' ker])
+end
+end
+
+function D = norm2mat(X1,X2)
+n1sq = sum(X1.^2,1);
+n1 = size(X1,2);
+
+if exist('X2','var');
+    n2sq = sum(X2.^2,1);
+    n2 = size(X2,2);
+    D = n1sq'*ones(1,n2) + ones(n1,1)*n2sq - 2*(X1'*X2);
+    % In MATLAB multipliying by 'ones' is faster than repmat!
+    % D = repmat(n1sq',1,n2) + repmat(n2sq,n1,1) - 2*(X'*X2);
+else
+    D = 2 * (ones(n1,1)*n1sq - X1'*X1);
+    % D = 2 * (repmat(n1s1,n1,1) - X'*X);
+end
 end
