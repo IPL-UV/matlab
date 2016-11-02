661d30a2645546920e9df444f7701a3bbc6c1d27
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-11-02 15:21:17 +0000

    Added new trainSVMc for SVM classification, and related files. In regression toolbox, fixex trainTREE, testTREE and trainNN.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@658 cf90e129-9db2-4be2-879b-6cf61410cf68

--- folds.m
+++ folds.m
@@ -4,7 +4,7 @@ function groups = folds(n, vf)
 %
 % Creates the vf groups for a length n
 
-s = rand('state');
-rand('state',0);
-groups = ceil(vf*randperm(n)/n);
-rand('state',s);
+s = rng;
+rng(0);
+groups = ceil(vf * randperm(n) / n);
+rng(s);
