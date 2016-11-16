92cd03f1417072197d030fc346af1c451cb18594
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-11-16 09:17:54 +0000

    Substituted repeated functions in toolboxes (i.e. assessment.m, estimateSigma.m, kernelmatrix.m, etc.) by symlinks to ./Matlab/ corresponding functions.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@663 cf90e129-9db2-4be2-879b-6cf61410cf68

--- assessment.m
+++ assessment.m
@@ -1,6 +1,5 @@
 function RESULTS = assessment(Labels,PreLabels,par)
 
-%
 %   function RESULTS = assessment(Labels,PreLabels,par)
 %
 %   INPUTS:
@@ -25,7 +24,6 @@ function RESULTS = assessment(Labels,PreLabels,par)
 %   Formulae in:
 %   Assessing the Accuracy of Remotely Sensed Data
 %   by Russell G Congalton, Kass Green. CRC Press
-%
 
 switch lower(par)
 	case {'class'}
@@ -34,28 +32,28 @@ switch lower(par)
 	NumClases = length(Etiquetas); % Number of classes
 
 	% Compute confusion matrix
-    ConfusionMatrix = zeros(NumClases);
-	for i=1:NumClases
-	        for j=1:NumClases
+	ConfusionMatrix = zeros(NumClases);
+	for i = 1:NumClases
+	        for j = 1:NumClases
 	                ConfusionMatrix(i,j) = length(find(PreLabels==Etiquetas(i) & Labels==Etiquetas(j)));
-	        end;
-	end;
+	        end
+	end
 
 	% Compute Overall Accuracy and Cohen's kappa statistic
-	n      = sum(ConfusionMatrix(:));                     % Total number of samples
-	PA     = sum(diag(ConfusionMatrix));
-	OA     = PA/n;
+	n  = sum(ConfusionMatrix(:));                     % Total number of samples
+	PA = sum(diag(ConfusionMatrix));
+	OA = PA / n;
 
 	% Estimated Overall Cohen's Kappa (suboptimal implementation)
 	npj = sum(ConfusionMatrix,1);
 	nip = sum(ConfusionMatrix,2);
-	PE  = npj*nip;
+	PE  = npj * nip;
     if (n*PA-PE) == 0 && (n^2-PE) == 0
         % Solve indetermination
-        %warning('0 divided by 0')
+        % warning('0 divided by 0')
         Kappa = 1;
     else
-    	Kappa  = (n*PA-PE)/(n^2-PE);
+    	Kappa = (n*PA-PE)/(n^2-PE);
     end
 
 	% Cohen's Kappa Variance
@@ -64,38 +62,40 @@ switch lower(par)
 	theta3 = (nip'+npj) * diag(ConfusionMatrix)  / n^2;
 
 	suma4 = 0;
-	for i=1:NumClases
-	for j=1:NumClases
-		suma4 = suma4 + ConfusionMatrix(i,j)*(nip(i) + npj(j))^2;
-	end;
-	end;
-	theta4 = suma4/n^3;
-	varKappa = ( theta1*(1-theta1)/(1-theta2)^2     +     2*(1-theta1)*(2*theta1*theta2-theta3)/(1-theta2)^3      +     (1-theta1)^2*(theta4-4*theta2^2)/(1-theta2)^4  )/n;
-	Z = Kappa/sqrt(varKappa);
-	CI = [Kappa + 1.96*sqrt(varKappa), Kappa - 1.96*sqrt(varKappa)];
+	for i = 1:NumClases
+	    for j = 1:NumClases
+		    suma4 = suma4 + ConfusionMatrix(i,j) * (nip(i) + npj(j))^2;
+	    end
+	end
+	theta4 = suma4 / n^3;
+	varKappa = ( theta1 * (1-theta1) / (1-theta2)^2 + ...
+	             2 * (1-theta1) * (2*theta1*theta2-theta3) / (1-theta2)^3 + ...
+				 (1-theta1)^2 * (theta4-4*theta2^2) / (1-theta2)^4 ) / n;
+	Z = Kappa / sqrt(varKappa);
+	CI = [Kappa + 1.96 * sqrt(varKappa), Kappa - 1.96 * sqrt(varKappa)];
 
-	if NumClases==2
+	if NumClases == 2
 	    % Wilcoxon test at 95% confidence interval
 	    [p1,h1] = signrank(Labels,PreLabels);
-	    if h1==0
+	    if h1 == 0
 	        RESULTS.WilcoxonComment	= 'The null hypothesis of both distributions come from the same median can be rejected at the 5% level.';
-	    elseif h1==1
+	    elseif h1 == 1
 	        RESULTS.WilcoxonComment	= 'The null hypothesis of both distributions come from the same median cannot be rejected at the 5% level.';
 	    end;
-	    RESULTS.WilcoxonP		= p1;
+	    RESULTS.WilcoxonP = p1;
 
 	    % Sign-test at 95% confidence interval
 	    [p2,h2] = signtest(Labels,PreLabels);
-	    if h2==0
+	    if h2 == 0
 	        RESULTS.SignTestComment	= 'The null hypothesis of both distributions come from the same median can be rejected at the 5% level.';
-	    elseif h2==1
+	    elseif h2 == 1
 	        RESULTS.SignTestComment	= 'The null hypothesis of both distributions come from the same median cannot be rejected at the 5% level.';
 	    end;
-	    RESULTS.SignTestP		= p2;
+	    RESULTS.SignTestP = p2;
 
 	    % McNemar
-	    RESULTS.Chi2 = (abs(ConfusionMatrix(1,2)-ConfusionMatrix(2,1))-1)^2/(ConfusionMatrix(1,2)+ConfusionMatrix(2,1));
-	    if RESULTS.Chi2<10
+	    RESULTS.Chi2 = (abs(ConfusionMatrix(1,2) - ConfusionMatrix(2,1)) - 1)^2 / (ConfusionMatrix(1,2) + ConfusionMatrix(2,1));
+	    if RESULTS.Chi2 < 10
 	        RESULTS.Chi2Comments = 'The Chi^2 is not approximated by the chi-square distribution. Instead, the sign test should be used.';
 	    else
 	        RESULTS.Chi2Comments = 'The Chi^2 is approximated by the chi-square distribution. The greater Chi^2, the lower p<0.05 and thus the difference is more statistically significant.';
@@ -105,7 +105,7 @@ switch lower(par)
 	% Store results:
 	RESULTS.ConfusionMatrix = ConfusionMatrix;
 	RESULTS.Kappa           = Kappa;
-	RESULTS.OA              = 100*OA;
+	RESULTS.OA              = 100 * OA;
 	RESULTS.varKappa        = varKappa;
 	RESULTS.Z               = Z;
 	RESULTS.CI              = CI;
@@ -114,23 +114,21 @@ case 'regress'
 
 	RESULTS.ME   = mean(Labels-PreLabels);
 	RESULTS.RMSE = sqrt(mean((Labels-PreLabels).^2));
-    RESULTS.RELRMSE = RESULTS.RMSE/mean(PreLabels);
+        RESULTS.RELRMSE = RESULTS.RMSE/mean(PreLabels);
 	RESULTS.MAE  = mean(abs(Labels-PreLabels));
-    if length(Labels) == 1
-        % Correlation coefficient does not make sense for one point
-        RESULTS.R    = 0;
-        RESULTS.R2   = 0;
-    else
-        %[rr, pp]     = corrcoef(Labels,PreLabels);
-        rr           = corrcoef(Labels,PreLabels);
-        RESULTS.R    = rr(1,2);
-        RESULTS.R2   = rr(1,2)^2;
-        %RESULTS.RP   = pp(1,2);
-    end
+        if length(Labels) == 1
+            % Correlation coefficient does not make sense for one point
+            RESULTS.R    = 0;
+            RESULTS.R2   = 0;
+        else
+            %[rr, pp]     = corrcoef(Labels,PreLabels);
+            rr           = corrcoef(Labels,PreLabels);
+            RESULTS.R    = rr(1,2);
+            RESULTS.R2   = rr(1,2)^2;
+            %RESULTS.RP   = pp(1,2);
+        end
 
 otherwise
 	disp('Unknown learning paradigm.')
 end
-
-
 
