6f58e37cc6b69a7300e65f22aa11ff6c14c18e04
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-12-27 15:30:51 +0000

    Fixed bug in trainNN.m introduced in previous commit. Added nice new colormap (cmr_m2k16.m) since MATLAB > R2015 to use in previous versions.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@664 cf90e129-9db2-4be2-879b-6cf61410cf68

--- cmr.m
+++ cmr.m
@@ -1,8 +1,9 @@
 function h = cmr(m)
 
-%"A Color Map for Effective Black-and-white Rendering of Color-Scale Images", Carey Rappaport, IEEE Antennas and Propagation Magazine, Vol. 44, No. 3, June 2002
+% "A Color Map for Effective Black-and-white Rendering of Color-Scale Images",
+% Carey Rappaport, IEEE Antennas and Propagation Magazine, Vol. 44, No. 3, June 2002
 
-%CMR    Color Map Rendering for Black-and-white Images
+% CMR  Color Map Rendering for Black-and-white Images
 %   CMR(M) returns an M-by-3 matrix containing a "cmr" colormap.
 %   CMR, by itself, is the same length as the current figure's
 %   colormap. If no figure exists, MATLAB creates one.
@@ -16,16 +17,14 @@ function h = cmr(m)
 
 if nargin < 1, m = size(get(gcf,'colormap'),1); end
 
-h = [
-0.00 0.00 0.00;
-0.15 0.15 0.50;
-0.30 0.15 0.75;
-0.60 0.20 0.50;
-1.00 0.25 0.15;
-0.90 0.50 0.00;
-0.90 0.75 0.10;
-0.90 0.90 0.50;
-1.00 1.00 1.00];
+h = [ 0.00 0.00 0.00;
+      0.15 0.15 0.50;
+      0.30 0.15 0.75;
+      0.60 0.20 0.50;
+      1.00 0.25 0.15;
+      0.90 0.50 0.00;
+      0.90 0.75 0.10;
+      0.90 0.90 0.50;
+      1.00 1.00 1.00 ];
 
-h=interp1(1:size(h,1),h,linspace(1,size(h,1),m));
-
+h = interp1(1:size(h,1), h, linspace(1,size(h,1), m));
